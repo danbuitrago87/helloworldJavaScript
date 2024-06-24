2acc758448e2db2486952bde90e9cc734a18f51e
unknown <danbuitrago87@gmail.com>
2024-06-24 16:48:35 -0500

    SecondCommit let console.log prompt

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('Hola mundo'); 
+let numSecret=5; //declaración de variable
+//pedimos al usuarioun número
+let numeroUsuario=prompt("Digita un número por favor: ")
+console.log(numeroUsuario)
+if (numSecret==numeroUsuario){
+    alert("El número es correcto...");
+} else{
+    alert("El número es incorrecto...");
+}
