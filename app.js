c12f3b9acce096286d6668e0e6153b888fc044fa
unknown <danbuitrago87@gmail.com>
2024-06-26 10:31:04 -0500

    FourthCommit comillas invertidas y muestra de valores de vbles

--- app.js
+++ app.js
@@ -5,5 +5,7 @@ console.log(numeroUsuario)
 if (numSecret==numeroUsuario){
     alert("El número es correcto...");
 } else{
-    alert("El número es incorrecto...");
+    //    DOS MANERAS DE MOSTRAR VALORES GUARDADOS
+    //alert("El número que elegiste es: " + numeroUsuario + " \nEl número correcto es: " + numSecret);
+    alert(`El número que elegiste es: ${numeroUsuario} \nEl número correcto es: ${numSecret}`);
 }
